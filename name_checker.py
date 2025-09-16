01b41893d7b12a0824fd894e6a88ffeee5799c09
Oyewole Rofeeq <oyewolerofeeq2019@gmail.com>
2025-09-16 05:58:29 -0700

    First commit

---
+++ name_checker.py
@@ -0,0 +1,18 @@
+
+name = ("Mr Nas")
+if  name == "Mr Nas":
+    print("Hello Facility Manager")
+elif name == "Mr Ahmed":
+    print("Hey Admin Manager")
+elif name == "Mr Rofeeq":
+    print("Hii Corp Member")
+elif name == "Mr Quardri":
+    print("Safety Officer")
+elif name == "Sece":
+    print("Hii Secretary")
+elif name == "Mr Habeeb":
+    print("Hii Administrator")
+else:
+    print("Sorry, I don't Know you!")
+
+
